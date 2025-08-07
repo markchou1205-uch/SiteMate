bb40a4228ad4da76534c476fa7a4f8a1ded98ed6
馬克 <teachermarkchou@gmail.com>
2025-08-07 14:55:52 +0000

    chore: add .gitignore and remove .next from repo

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app/edit-pdf/page.tsx -> ./PdfEditor\":{\"id\":\"app/edit-pdf/page.tsx -> ./PdfEditor\",\"files\":[\"static/chunks/_app-pages-browser_src_app_edit-pdf_PdfEditor_tsx.js\"]}}"
